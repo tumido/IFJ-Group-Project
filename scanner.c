fd65bbbac8198756384f5fef95b32bd152ad7e3d
healtonn <jochlik@email.cz>
2014-11-15 19:34:34 +0100

    prvni forma
    Momentálně tam nejsou všechny case-y, nicméně princip bude stejnej. mám
    v plánu ve stavu s_lex_end prekoirovat vsechny udaje do mista, kam mi
    bude prideleno a tim vyplnit token.

--- scanner.c
+++ scanner.c
@@ -7,11 +7,147 @@
  *        Projekt:  IFJ
  * =====================================================================
  */
+#include <stdio.h>
+#include <ctype.h>
+#include "scanner.h"
 
-//test gitu
 
-void test
+#define DETAIL_LENGHT 8             //konstatna, kterou urcuji po kolika bytech bude provadena alokace pameti pro detail dokenu
+#define ERROR 1
+#define SUCCES 0
+
+FILE *Code;
+
+void load_Code (FILE *f)        //funkce pro nacteni ydrojoveho kodu
 {
-    printf ("test");
-    printf ("pokracovani v testu");
+  Code = f;
+}
+
+int token_detail_init(tDetail *str)
+{
+   if ((str->detail = (char*) malloc (DETAIL_LENGHT)) == NULL)
+    return ERROR;
+   str->detail[0] = '\0';
+   str->detail_lenght = 0;
+   str->allocated_memory = DETAIL_LENGHT;
+   return SUCCES;
+}
+
+void token_detail_free(tDetail *str)
+{
+    free(str->detail);
+}
+
+void token_detail_clean(tDetail *str)
+{
+    str->detail[0] = '\0'
+    str->detail_lenght = 0;
+}
+
+int add_string_to_char(tDetail *str, char z)
+{
+    if(str->detail_lenght + 1 >= str->allocated_memory)
+    {
+        if((str->detail = (char*) realloc(str->detail, str->detail_lenght + DETAIL_LENGHT)) == NULL)            //realokace pameti
+            return ERROR;
+        str->allocated_memory = str->detail_lenght + DETAIL_LENGHT;
+    }
+    str->detail[str->detail_lenght] = z;                    //pridani znaku do retezce
+    str->detail_lenght++;                                   //zvetseni velikosti retezce
+    str->detail[str->detail_lenght] = '\0'                  //pridani znaku 0 na konec
+}
+
+
+void fill_Token()               //funkce, ktera naplni token
+{
+
+tDetail detail_tokenu;         //promena, do ktere budu ukladat jak se danny identifikator jmenuje
+tState state = s_begin         //nastavim pocatecni stav automatu
+bool read = true;              //bool pro zastaveni automatu
+int z;                         //promenna pro nacitani pismen/znaku
+
+token_detail_init(&detail_tokenu);
+
+  while (read && (z = getc(Code)))      //start automatu
+   {
+      switch(state)
+      {
+         case s_begin:                                          //pocatecni stav automatu, samotny switch bude v ramci moznosti formou seznamu, za ucelem zprehledneni
+         {
+           if (isspace(z))                          state = s_begin;        //bile znaky preskocit
+           else if (z == '{')                       state = s_coment;       //narazil jsem na "{" -> zacina komentar
+           else if (isalpha(z) || z == '_')         state = s_id;           //pismeno nebo podtrzitko znaci zacatek identifikatoru nebo klicoveho slova
+           else if (isdigit(z))                     state = s_integer;      //cele nebo desetinne cislo
+           else if (z == 39)                        state = s_string;       //zacatek retezce, cislo 39 je ascii hodnota apostrofu
+           else if (z == '+')                       state = s_add_num;      //scitani
+           else if (z == '-')                       state = s_sub_num;      //odcitani
+           else if (z == '*')                       state = s_mul_num;      //nasobeni
+           else if (z == '/')                       state = s_mul_num;      //deleni
+           else if (z == '<')                       state = s_less;         //je mensi
+           else if (z == '>')                       state = s_bigger;       //je vetsi
+           else if (z == '=')                       state = s_same;         //je rovno
+           else if (z == ';')                       state = s_semicolon;    //strednik
+           else
+           {
+              state = s_lex_err;                                            //nastala chyba
+           }
+           add_string_to_char(detail_tokenu, z);                            //ulozeni nacteneho znaku
+           break;
+         }
+
+         case s_coment:                     //nacitam komentare, nic nedelam
+         {
+           if (z != '}');
+           else
+           {
+             state = s_begin;               //konec komentare
+           }
+           break;
+         }
+
+         case s_id;
+         {
+           if (isalnum(z) || z == '_')          //dentifikator dale muze obahovat jakekoli cislo ci pismeno a znak "_"
+           {
+             add_string_to_char(z);             //zapisu dalsi znak a pokracuju
+           }
+           else
+           {
+             state = s_lex_end;
+             ungetc(z);
+           }
+           break;
+         }
+
+         case s_integer:                        //nacitam cislo
+         {
+            if (isdigit(z))                     //pokracuje cislo
+                add_string_to_char(z);
+            if (z == 'E' || z == 'e')            //nacetl jsem exponent
+            {
+               add_string_to_char(z);
+               state = s_real_exp;
+            }
+            if (z == '.')
+            {
+               add_string_to_char(z);
+               state = s_real;
+            }
+            else
+            {
+               state = s_lex_end;
+               ungetc(z);
+            }
+            break;
+         }
+
+
+
+
+
+
+
+      }
+   }
+
 }
--- scanner.h
+++ scanner.h
@@ -2,10 +2,53 @@
  * =====================================================================
  *          Verze:  1.0
  *      Vytvoreno:  11/12/2014 04:23:21 PM
- *         Autori:  Tom√°≈° Coufal, Roman Hal√≠k, Yurij Hladyuk, Jakub Jochl√≠k
+ *         Autori:  Tom·ö Coufal, Roman HalÌk, Yurij Hladyuk, Jakub JochlÌk
  *          Login:  xcoufa09, xhalik01, xhlady00, xjochl00
  *        Projekt:  IFJ
  * =====================================================================
  */
 
 
+
+
+typedef enum            //seznam vsech stavu automatu
+{
+   s_begin,             // 0 pocatecni stav automatu
+   s_coment,            // 1 komentare
+   s_id,                // 2 identifikator
+   s_integer,           // 3 cislo
+   s_real,              // 4 desetinne cislo
+   s_string,            // 5 retezec
+   s_add_num,           // 6 operator +
+   s_sub_num,           // 7 operator -
+   s_mul_num,           // 8 operator *
+   s_div_num,           // 9 operator /, vysledek je vzdy typu real!
+   s_less,              //10 operator <
+   s_bigger,            //11 operator >
+   s_less_or,           //12 operator <=
+   s_bigger_or          //13 operator >=
+   s_same,              //14 operator =
+   s_not_same,          //15 operator <>
+   s_semicolon,         //16 strednik ;
+   s_lex_end,           //17 konec tvorby tokenu
+   s_lex_err,           //18 chyba, ktera vznikla pri lex analyze
+   s_real_exp,          //19 pokracovani desetinneho cisla ..... 10e-2
+   s_real_exp_all       //20 pokracovani desetinneho cisla ..... 10.41e-2
+
+}tState;
+
+typedef struct
+{
+   char* detail;            //detaily k identifikatoru
+   int detail_lenght;       //delka retezece (detailu)
+   int allocated_memory;    //velikost allokovane pameti
+
+}tDetail;
+
+
+void fill_Token();                //predpokladam, ze budu dostavat adresu, kde bude prazdny token(formou struktury?) k vyplneni
+void load_Code (FILE *f);
+int token_detail_init(tDetail *str);                    //inicializace detailu k ID
+void token_detail_free(tDetail *str);                   //uvolneni pameti
+void token_detail_clean(tDetail *str);                  //promazani detailu
+int add_string_to_char(tDetail *str, char z);           //prida znak do existujiciho retezce
