72ebf0f77d469315742767732b84e909851cfa59
Roman Halík <rhalik@seznam.cz>
2014-11-21 17:06:02 +0100

    soubory na testování lexikální analýzy

---
+++ Testing files/lexicalTest1.pas
@@ -0,0 +1,29 @@
+if 1 begin		
+var a94_94 := 11;
+end;  
+'a'
+'nazdarnazdar nazdar NAZDAR, NAzdAR'
+{
+
+
+komentar komentar komentar
+}
+
+var i = {komentar}100;
+  begin
+    ;;;;;;
+  end;
+function testTest (id_parametr : typ) : void;
+var aba;
+var aca;{komentar}
+begin
+  var aba := var aca;
+;;
+end;
+while	 true	 do 	begin 	var lama := 'lama'; end;;;
+{operatory} +-*/=>=<<>==
+  {cisilka}12e153.111
+{vestavene fce} ``	lenght(); copy(); find(); sort();
+{retezcovy literal} '#255' '#90'   '###' 'je apostrof v retezci '' ?'
+
+write "%g"
