c2452614436a258762783a61af758b3f8d2cff65
healtonn <jochlik@email.cz>
2014-11-22 19:58:39 +0100

    Uprava souboru podle patecni domluvy
    Funkce fillToken nyni ocekava ukazatel na soubor, pridan chybejici
    stav
    s_not_same, korekce chyb, pridana funkce na rozpoznani klicovych
    slov,
    pridana "lexType tokenMain" do struktury token.

--- scanner.c
+++ scanner.c
@@ -2,7 +2,7 @@
  * =====================================================================
  *          Verze:  1.0
  *      Vytvoreno:  11/12/2014 04:21:31 PM
- *         Autori:  TomÃ¡Å¡ Coufal, Roman HalÃ­k, Yurij Hladyuk, Jakub JochlÃ­k
+ *         Autori:  Tomáš Coufal, Roman Halík, Yurij Hladyuk, Jakub Jochlík
  *          Login:  xcoufa09, xhalik01, xhlady00, xjochl00
  *        Projekt:  IFJ
  * =====================================================================
@@ -44,7 +44,41 @@ int addCharToString(token *str, char z)
   return EXIT_SUCCESS;
 }
 
-void fillToken()               //funkce, ktera naplni token
+/* Funkce pro kontrolu klicovych slov, jako parametr predavam ukazatel na token (strukturu)
+ * Pomoci strmp() a vetveni if else se rozhodnu, zdali se jedna o identifikator ci klicove slovo
+ * a vysledek zapisu do tokenu
+ */
+int keyWordCheck(token *str)
+{
+  int result;
+  if((strcmp(str->detail, "begin")) == 0)             str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "boolean")) == 0)      str->tokenMain = l_bool;
+  else if((strcmp(str->detail, "do")) == 0)           str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "else")) == 0)         str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "end")) == 0)          str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "false")) == 0)        str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "find")) == 0)         str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "forward")) == 0)      str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "function")) == 0)     str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "if")) == 0)           str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "integer")) == 0)      str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "readln")) == 0)       str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "real")) == 0)         str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "sort")) == 0)         str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "string")) == 0)       str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "then")) == 0)         str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "true")) == 0)         str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "var")) == 0)          str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "while")) == 0)        str->tokenMain = l_keyWord;
+  else if((strcmp(str->detail, "write")) == 0)        str->tokenMain = l_keyWord;
+  else                                                // nejedna se o klicove slovo
+  {
+    str->tokenMain = l_id;
+  }
+  return 0;
+}
+
+void fillToken(FILE *Code)               //funkce, ktera naplni token
 {
   token detailTokenu;         //promena, do ktere budu ukladat jak se danny identifikator jmenuje
   tState state = s_begin;        //nastavim pocatecni stav automatu
@@ -52,7 +86,7 @@ void fillToken()               //funkce, ktera naplni token
   int z;                         //promenna pro nacitani pismen/znaku
   tokenDetailInit(&detailTokenu);
 
-  while (read && (z = getc(Code)))      //start automatu
+  while (read && (z = getc(*Code)))      //start automatu
   {
     switch(state)
     {
@@ -61,15 +95,39 @@ void fillToken()               //funkce, ktera naplni token
         else if (z == '{')                       state = s_coment;       //narazil jsem na "{" -> zacina komentar
         else if (isalpha(z) || z == '_')         state = s_id;           //pismeno nebo podtrzitko znaci zacatek identifikatoru nebo klicoveho slova
         else if (isdigit(z))                     state = s_integer;      //cele nebo desetinne cislo
-        else if (z == '\'')                        state = s_string;       //zacatek retezce, cislo 39 je ascii hodnota apostrofu
-        else if (z == '+')                       state = s_add_num;      //scitani
-        else if (z == '-')                       state = s_sub_num;      //odcitani
-        else if (z == '*')                       state = s_mul_num;      //nasobeni
-        else if (z == '/')                       state = s_mul_num;      //deleni
+        else if (z == '\'')                      state = s_string;       //zacatek retezce
+        else if (z == '+')
+        {
+          state = s_add_num;                                             //scitani
+          detailTokenu->tokenMain = l_add;
+        }
+        else if (z == '-')
+        {
+          state = s_sub_num;                                             //odcitani
+          detailTokenu->tokenMain = l_sub;
+        }
+        else if (z == '*')
+        {
+          state = s_mul_num;                                             //nasobeni
+          detailTokenu->tokenMain = l_mul;
+        }
+        else if (z == '/')
+        {
+          state = s_div_num;                                             //deleni
+          detailTokenu->tokenMain = l_div;
+        }
         else if (z == '<')                       state = s_less;         //je mensi
         else if (z == '>')                       state = s_bigger;       //je vetsi
-        else if (z == '=')                       state = s_same;         //je rovno
-        else if (z == ';')                       state = s_semicolon;    //strednik
+        else if (z == '=')
+        {
+          state = s_same;                                                //je rovno
+          detailTokenu->tokenMain = l_same;
+        }
+        else if (z == ';')
+        {
+          state = s_semicolon;                                           //strednik
+          detailTokenu->tokenMain = l_semicolon;
+        }
         else
         {
           state = s_lex_err;                                            //nastala chyba
@@ -90,6 +148,7 @@ void fillToken()               //funkce, ktera naplni token
         }
         else
         {
+          keyWordCheck(&detailTokenu);
           state = s_lex_end;
           ungetc(z);
         }
@@ -97,18 +156,19 @@ void fillToken()               //funkce, ktera naplni token
       case s_integer:                        //nacitam cislo
         if (isdigit(z))                     //pokracuje cislo
             addCharToString(&detailTokenu, z);
-        if (z == 'E' || z == 'e')            //nacetl jsem exponent
+        else if (z == 'E' || z == 'e')            //nacetl jsem exponent
         {
           addCharToString(&detailTokenu, z);
           state = s_real_exp;
         }
-        if (z == '.')
+        else if (z == '.')
         {
           addCharToString(&detailTokenu, z);
           state = s_real;
         }
         else
         {
+          detailTokenu->tokenMain = l_integer;
           state = s_lex_end;
           ungetc(z);
         }
@@ -135,6 +195,7 @@ void fillToken()               //funkce, ktera naplni token
         }
         else
         {
+          detailTokenu->tokenMain = l_real;
           state = s_lex_end;
           ungetc(z, Code);
         }
@@ -173,56 +234,60 @@ void fillToken()               //funkce, ktera naplni token
         }
         else
         {
+          detailTokenu->tokenMain = l_real;
           state = s_lex_end;
           ungetc(z, Code);
         }
         break;
       case s_string:                         //nacitam retezec ohraniceny apostrofy
-        if (z == 39)
+        addCharToString(&detailTokenu, z);
+        if (z == '\'')
         {
-          addCharToString(&detailTokenu, z);
           state = s_string_check;
         }
-        else
-        {
-          addCharToString(&detailTokenu, z);
-        }
+
         break;
-      case s_string_check:                   //nacetl jsem jeden apostrof, pokud nactu druhy, jsem stale v retezci, pokud ne, retezec konci
-        if(z == 39)
+      case s_string_check:                   //nacetl jsem jeden apostrof, pokud nactu druhy, jsem stale v retezci, pokud ne, retezec konci. Zaroven dalsi apostrof neukladam
+        if(z == '\'')
         {
-          addCharToString(&detailTokenu, z);
           state = s_string;
         }
         else
         {
-          addCharToString(&detailTokenu, z);
+          detailTokenu->tokenMain = l_string;
           state = s_lex_end;
           ungetc(z, Code);
         }
         break;
       case s_less:           //nacetl jsem < .... je mensi
-        if (z == '=')    //jeste muze prijit =
+        addCharToString(&detailTokenu, z);
+        if (z == '=')    //jeste muze prijit = nebo >
         {
-          addCharToString(&detailTokenu, z);
+          detailTokenu->tokenMain = l_lessOr;
           state = s_less_or;
+        }
+        else if(z == '>')
+        {
+          state = s_not_same;
+          detailTokenu->tokenMain = l_notSame:
         }
         else
         {
-          addCharToString(&detailTokenu, z);
+          detailTokenu->tokenMain = l_less;
           state = s_lex_end;
           ungetc(z, Code);
         }
         break;
       case s_bigger:
+        addCharToString(&detailTokenu, z);
         if (z == '=')
         {
-          addCharToString(&detailTokenu, z);
+          detailTokenu->tokenMain = l_biggerOr;
           state = s_bigger_or;
         }
         else
         {
-          addCharToString(&detailTokenu, z);
+          detailTokenu->tokenMain = l_bigger;
           state = s_lex_end;
           ungetc(z, Code);
         }
@@ -233,9 +298,9 @@ void fillToken()               //funkce, ktera naplni token
       case s_div_num:
       case s_semicolon:
       case s_same:
+      case s_not_same:
       case s_less_or:
       case s_bigger_or:
-        addCharToString(&detailTokenu, z);
         state = s_lex_end;
         ungetc(z, Code);
         break;
@@ -244,6 +309,7 @@ void fillToken()               //funkce, ktera naplni token
         read = false;
         break;
       case s_lex_err:                    //chyba pri lexikalni analyze
+        return EXIT_LEXICAL_ERROR;
     }
   }
 }
--- scanner.h
+++ scanner.h
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <string.h>
 #include "io.h"
 
 #define DETAIL_LENGHT 8             //konstatna, kterou urcuji po kolika bytech bude provadena alokace pameti pro detail dokenu
@@ -33,7 +34,7 @@ typedef enum            //seznam vsech stavu automatu
   s_less,              //10 operator <
   s_bigger,            //11 operator >
   s_less_or,           //12 operator <=
-  s_bigger_or,          //13 operator >=
+  s_bigger_or,         //13 operator >=
   s_same,              //14 operator =
   s_not_same,          //15 operator <>
   s_semicolon,         //16 strednik ;
@@ -43,19 +44,42 @@ typedef enum            //seznam vsech stavu automatu
   s_real_exp_all       //20 pokracovani desetinneho cisla ..... 10.41e-2
 } tState;
 
-typedef struct
+typedef enum           //Pomoci tohoto enumerate budu ukladat informaci o lexemu do tokenu ---zdali se jedna o indetifikator,integer,etc
 {
-  char * detail;            //detaily k identifikatoru
+  l_id,                 // 0 nacetl jsem identifikator
+  l_keyWord,            // 1 nacetl jsem klicove slovo
+  l_integer,            // 2 nacetl jsem integer
+  l_real,               // 3 nacetl jsem real
+  l_string,             // 4 nacetl jsem retezcovy literal
+  l_bool,               // 5 nacetl jsem boolovsky literal
+  l_add,                // 6 nacetl jsem scitani
+  l_sub,                // 7 nacetl jsem odcitani
+  l_mul,                // 8 nacetl jsem nasobeni
+  l_div,                // 9 nacetl jsem deleni
+  l_less,               //10 nacetl jsem mensi
+  l_lessOr,             //11 nacetl jsem mensi rovno
+  l_bigger,             //12 nacetl jsem vetsi
+  l_biggerOr,           //13 nacetl jsem vetsi rovno
+  l_same,               //14 nacetl jsem rovna se
+  l_notSame,            //15 nacetl jsem nerovna se
+  l_semicolon,          //16 nacetl jsem strednik
+} lexType;
+
+typedef struct            //Samotny token, jeho struktura
+{
+  lexType tokenMain;      //Hlavni cast tokenu, yde ukladam, co jsem vlastne nacetl za typ lexemu
+  char * detail;          //detaily k lexemu
   int detailLenght;       //delka retezece (detailu)
   int allocatedMemory;    //velikost allokovane pameti
 
 } token;
 
 
-void fillToken();                //predpokladam, ze budu dostavat adresu, kde bude prazdny token(formou struktury?) k vyplneni
+void fillToken(FILE *Code);                //predpokladam, ze budu dostavat adresu, kde bude prazdny token(formou struktury?) k vyplneni
 int tokenDetailInit(token *str);                    //inicializace detailu k ID
 void tokenDetailFree(token *str);                   //uvolneni pameti
 void tokenDetailClean(token *str);                  //promazani detailu
 int addCharToString(token *str, char z);           //prida znak do existujiciho retezce
+int keyWordCheck(token *str);
 
 #endif
