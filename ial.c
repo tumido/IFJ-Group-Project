77423a27af6ef520cf90e7de16a3953405095295
Roman Halík <rhalik@seznam.cz>
2014-12-07 21:35:02 +0100

    Funkce FIND pomoci Boyer-Mooreova algoritmu, zatim netestovano

--- ial.c
+++ ial.c
@@ -7,13 +7,14 @@
  *        Projekt:  IFJ
  * =====================================================================
  */
-
+#include "ial.h"
+#include "io.h"
 
 /*
  * Funkce shellSort
  * -----------------------------------------------------------------------------
  * - provadi shellovo razeni n prvku v poli
- * - implemetace podle slajdu prof. Hozníka
+ * - implemetace podle slajdu prof. Hoznika
  * - zatim neotestovano
  * 
  * - serazene znaky budu rovnou vypisovat na stdout, nebo je nekam ulozim, ha?
@@ -32,7 +33,7 @@ void shellSort(char *array, int n)
 
       while ((j >= 1) && (array[j] > array[j + step])) // bubble pruchod
       {
-        tmp = a[j];
+        tmp = array[j];
         array[j] = array[j + step];
         array[j + step] = tmp;
         j = j - step; // snizeni indexu o krok
@@ -45,7 +46,118 @@ void shellSort(char *array, int n)
 /*
  * Funkce findSubString
  * -----------------------------------------------------------------------------
- * - vyhledava podretezec zadane delky 'n' v retezci 's' 
- * - implementovano pomoci boyer-mooreova algoritmu
- */ 
-findSubtring();
+ * - vyhledava podretezec zadane delky 'n' v retezci 's'
+ * - implementovano pomoci boyer-mooreova algoritmu:
+ *   - prochazi text zleva doprava a porovnava vzor s textem zprava doleva
+ *   - jestlize znak na aktualni pozici ve vzorku neni (pozna na zaklade tabulky
+ *     spatnych znaku), preskoci na index vetsi o delku vzorku
+ *   - jestlize znak na akutali pozici ve vzorku je, porovnava nasledujici znaky
+ *     zprava doleva
+ *   - pro zabezpeceni rozeznani opakujicich se posloupnosti znaku ve vzoru slouzi
+ *     tabulka 'dobrych' suffixu
+ */
+
+/*
+ * badCharacters
+ * - naplni pole badChars 'spatnymi' znaky
+ */
+void badCharacters(char *pattern, int patternLenght, int badChars[])
+{
+  int i;
+
+  for (i = 0; i < SIZE; ++i) badChars[i] = patternLenght;
+  for (i = 0; i < patternLenght - 1; ++i) badChars[pattern[i]] = patternLenght - i - 1;
+}
+
+/*
+ * suffixes
+ * - vypocte suffixy pro zadany vzor (pattern)
+ */
+void suffixes(char *pattern, int patternLenght, int *suffix)
+{
+  int f, g, i;
+
+  suffix[patternLenght - 1] = patternLenght;
+  g = patternLenght - 1;
+
+  for (i = patternLenght - 2; i >= 0; --i)
+  {
+    if ((i > g) && (suffix[i + patternLenght - 1 - f]) < (i - g))
+      suffix[i] = suffix[i + patternLenght - 1 - f];
+    else
+    {
+      if (i < g) g = i;
+
+      f = i;
+
+      while ((g >= 0) && (pattern[g] == pattern[g + patternLenght - 1 - f])) --g;
+
+      suffix[i] = f - g;
+    }
+  }
+}
+
+/*
+ * goodSuffixes
+ * - naplni pole spravnymi suffixy
+ */
+void goodSuffixes(char *pattern, int patternLenght, int goodSuffix[])
+{
+  int i = 0;
+  int j = 0;
+  int suffix[SIZE];
+
+  suffixes(pattern, patternLenght, suffix);
+
+  for (i = 0; i < patternLenght; ++i) goodSuffix[i] = patternLenght;
+
+  for (i = patternLenght - 1; i >= 0; --i)
+  {
+    if (suffix[i] == i + 1)
+    {
+      for (; j < patternLenght - 1 - i; ++j)
+      {
+        if (goodSuffix[j] == patternLenght) goodSuffix[j] = patternLenght - 1 - i;
+      }
+    }
+  }
+
+  for (i = 0; i <= patternLenght - 2; ++i)
+    goodSuffix[patternLenght - 1 - suffix[i]] = patternLenght - 1 - i;
+}
+
+/*
+ * findSubString
+ * - hlavni ridici funkce celeho algoritmu, spousti pomocne funkce
+ * - vraci index prvniho vyskytu zadaneho podretezce, nebo by alespon mela..
+ */
+int findSubString(char *pattern, char *text)
+{
+  int rowLenght = strlen(text);
+  int patternLenght = strlen(pattern);
+  int i, j, count, goodSuffix[SIZE], badChars[SIZE];
+
+  // vytvoreni tabulek
+  goodSuffixes(pattern, patternLenght, goodSuffix);
+  badCharacters(pattern, patternLenght, badChars);
+
+  // hledani
+  j = count = 0;
+  while (j <= rowLenght - patternLenght)
+  {
+    for (i = patternLenght - 1; i >= 0 && pattern[i] == text[i + j]; --i);
+    if (i < 0)
+    {
+      count++;
+      j += goodSuffix[0];
+    }
+    else
+    {
+      if (goodSuffix[i] < (badChars[text[i + j]] - patternLenght + 1 + i))
+        j += (badChars[text[i + j]] - patternLenght + 1 + i);
+      else j += goodSuffix[i];
+    }
+  }
+  return count + 1; // nutna korekce, pozice je pocitana od 1
+  // jeste nutno vyresit vraceni 0 pri nenalezeni retezce, ale napret otestovat a ono se na to prijde..
+}
--- ial.h
+++ ial.h
@@ -8,13 +8,41 @@
  * =====================================================================
  */
 
+// maximalni delka radky zpracovavaneho retezce, nad timto se bude treba
+// jeste zamyslet..
+#define SIZE 255 
+
+
 /*
  * Funkce vykonavajici radici algoritmus shell sort
  */
- void shellSort(int *array, int n);
+void shellSort(int *array, int n);
 
- /*
- * Funkce vyhledavajici podretezec v zadanem retezci pomoci boyer-mooreova alg.
+/*
+ * badCharacters
+ * - pomocna funkce BM algormitmu
+ * - naplni pole badChars 'spatnymi' znaky, tedy takovymi, ktere se nevyskytuji
+ *   ve hledanem podretezci
  */
- void findSubString();
- 
+void badCharacters(char *pattern, int patternLenght, int badChars[]);
+
+/*
+ * suffixes
+ * - pomocna funkce BM algormitmu
+ * - vypocte suffixy pro zadany vzor (pattern)
+ */
+void suffixes(char *pattern, int patternLenght, int *suffix);
+
+/*
+ * goodSuffixes
+ * - pomocna funkce BM algormitmu
+ * - naplni pole spravnymi suffixy
+ */
+void goodSuffixes(char *pattern, int patternLenght, int goodSuffix[]);
+
+/*
+ * findSubString
+ * - hlavni ridici funkce celeho Boyer-Mooreova algoritmu, spousti pomocne funkce
+ * - vraci index prvniho vyskytu zadaneho podretezce, nebo by alespon mela..
+ */
+int findSubString(char *pattern, char *text);
